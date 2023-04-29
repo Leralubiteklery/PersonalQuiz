94f0f147727d17b1e40de93dbe0e1d4c79a32165
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-04-29 13:27:03 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Lera Savchenko on 29.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
